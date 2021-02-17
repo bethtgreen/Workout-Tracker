c53a9a619b6b2a41c1a2c0220202fda234ef9384
Bethany Green <b.gree10@gail.com>
2021-02-16 23:05:12 -0600

    organizing files

--- server.js
+++ server.js
@@ -1,23 +1,21 @@
 const express = require("express");
-// add mongoose //
 const logger = require("morgan");
 const mongoose = require("mongoose");
-
-
+//port//
 const PORT = process.env.PORT || 3002;
 const app = express();
 // this requires the content inside of the models foldr
 const db = require("./models")
+app.use(logger("dev"));
 
 app.use(express.urlencoded({ extended : true }));
 //so we can use the public file
 app.use(express.static("public"));
 
 // requires the routes
-app.use(logger("dev"));
 app.use(require("./routes/htmlRoutes.js"));
 app.use(require("./routes/apiRoutes.js"));
-
+//helps connect to heroku//
 mongoose.connect( process.env.MONGODB_URI || "mongodb://localhost/agile-fortress", 
 {
   useNewUrlParser: true,
@@ -26,7 +24,7 @@ mongoose.connect( process.env.MONGODB_URI || "mongodb://localhost/agile-fortress
   useFindAndModify: false
   });
 
-
+//listens to port//
 app.listen(PORT, () => {
     console.log("Server listening on: http://localhost:" + PORT);
 });
