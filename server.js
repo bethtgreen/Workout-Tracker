f5a1332aa15404d8800e6f194748001569480aa1
Bethany Green <b.gree10@gail.com>
2021-02-16 22:16:09 -0600

    finishing up routes

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
 
+
 const PORT = 3002;
 const app = express();
 // this requires the content inside of the models foldr
@@ -12,9 +13,21 @@ app.use(express.urlencoded({ extended : true }));
 //so we can use the public file
 app.use(express.static("public"));
 
+// requires the routes
+app.use(logger("dev"));
 app.use("./routes/htmlRoutes");
-app.use("./routes/apiRoutes")
+app.use("./routes/apiRoutes");
+
+mongoose.connect( process.env.MONGODB_URI || "mongodb://localhost/workout", 
+{
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: false
+  });
+
 
 app.listen(PORT, () => {
     console.log("Server listening on: http://localhost:" + PORT);
-})
+});
+
