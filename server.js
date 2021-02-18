363c3bcdf4a406d03927946fc09f67759d5961c0
Bethany Green <b.gree10@gail.com>
2021-02-17 19:49:48 -0600

    deployed

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const logger = require("morgan");
 const Mongoose = require("mongoose");
 const PORT = process.env.PORT || 3000;
+
 const app = express();
 
 
@@ -9,6 +10,7 @@ app.use(logger("dev"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
+
 app.use(require("./routes/htmlRoutes"));
 app.use(require("./routes/apiRoutes"));
 
